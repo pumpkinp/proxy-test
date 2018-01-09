076c57071e14f0e11ff8c9449a7d40c7bf658dfd
pumpkinp <525168624@qq.com>
2018-01-09 16:55:26 +0800

    https测试

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: Http) {
+
+  }
+  url = 'http://localhost:4200/apiservice/ticker?key=fKzwJM05YBzhkKnRujj5vZPtnexjSz&label=ethbtc-ltcbtc&fiat=btc';
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe(res => {
+      console.info(res);
+    });
+  }
   title = 'app';
+
 }
